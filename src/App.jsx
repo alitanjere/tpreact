9260c60dfd6c64e5a4f7cc4919596351873c1bc2
KevinSebaLee <48044115@est.ort.edu.ar>
2025-04-24 11:27:00 -0300

    Avanzando

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+import TarjetaCita from './components/tarjetaCita/tarjetaCita';
+import CrearCita from './components/crearCita/crearCita';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [citas, setCitas] = useState([]);
+
+  const agregarCita = (nuevaCita) => {
+    setCitas([...citas, nuevaCita]);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <h1>Crear mi cita</h1>
+            <CrearCita crearCita={agregarCita} />
+          </div>
+
+          <div className="one-half column">
+            <h1>Administra tus citas</h1>
+            {citas.map((cita, numeroCitas) => (
+              <TarjetaCita 
+                key={numeroCitas}
+                nombre={cita.nombre}
+                dueño={cita.dueño}
+                fecha={cita.fecha}
+                hora={cita.hora}
+                sintomas={cita.sintomas}
+              />
+            ))}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
